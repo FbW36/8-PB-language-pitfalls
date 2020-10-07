b940149b6d339da04709d84f7daf1fff857f79b6
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-07 14:37:45 +0200

    First commit

---
+++ adrian/index.js
@@ -0,0 +1,13 @@
+//1
+// NaN stands for Not a Number.
+
+// "hello" is not NaN;
+console.log(Number.isNaN('hello'));
+
+//The type of NaN is "number".
+console.log(typeof NaN);
+
+//2
+console.log(Infinity / 0); // Infinity
+console.log(Infinity / Infinity); // NaN
+console.log('1' / '0'); // Infinity
