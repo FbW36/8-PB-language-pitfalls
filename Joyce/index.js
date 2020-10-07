eebe5d8bc6c5ce49ced95a8b4bbe49e031cb083a
Joyce <puch1996@gmail.com>
2020-10-07 14:24:30 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,17 @@
+// 1
+// NaN stands for Not a Number
+
+let value1 = "hello";
+let check1 = isNaN(value1);
+console.log(check1);
+
+let value2 = 3;
+let check2 = isNaN(value2);
+console.log(check2);
+
+// What is the type of NaN? ==> NaN is a number. However, it is a numeric data type whose value cannot be represented using actual numbers.
+
+// 2
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
