b52a44211e0d124dd33b1ffd4c69fd1850ffd67d
Muhasin <mahmudmuhasin@gmail.com>
2020-10-07 18:23:13 +0200

    first commit

---
+++ muhasin/index.js
@@ -0,0 +1,15 @@
+// 1. NaN
+let str = "hallo"
+let checkValue = isNaN(str);
+console.log("checkValue ==>", checkValue); // true
+
+let num = 3;
+let checkValue2 = isNaN(num);
+console.log("checkValue2 ==>", checkValue2); // false
+
+console.log(typeof (NaN)); // NaN is the type of number.
+
+// 2. Infinity
+console.log(Infinity / 0); // Infinity
+console.log(Infinity / Infinity);
+console.log(1 / 0);
