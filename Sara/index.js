20e3862e22f6e7600556f8275aa9c548fa3211d8
Sara <you@example.com>
2020-10-07 15:50:16 +0200

    finished exercise

---
+++ Sara/index.js
@@ -0,0 +1,32 @@
+// **Question: Comment your answer in your js file**: What does NaN stand for?  ❓ ❓ ❓ ❓ ❓ ❓ ❓ ❓ ❓ 
+
+// ==> NaN stands for Not-A-Number #️⃣  #️⃣  #️⃣  #️⃣ 
+
+// 1. NaN
+let value = 'Hello' ; // string
+let checkValue = isNaN(value) ; // checking if value is a number 
+console.log('value ==>', value) ; 
+console.log('checkValue ==>', checkValue);  
+
+let value2 = '3' ; // string number
+let checkValue2 = isNaN(value2) ; // checking if value2 is a number 
+console.log('value2 ==>', value2) ;
+console.log('checkValue2 ==>', checkValue2) ;
+
+// what is the type of NaN? ==> NaN (Not-A-Number) IS a Number! 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 🤣 
+
+// 2. Infinity ==> dividing any positive numbers with Infinity results in a positive 0
+console.log('0' / Infinity) ; // 0 //string number 
+console.log(0 / Infinity) ; // 0 // number 
+console.log(10 / Infinity) ; // 0
+console.log(20 / Infinity) ; // 0
+
+// divide Infinity with Infinity 
+console.log(Infinity / Infinity) ; // ==> NaN 
+
+// divide 1 by 0
+console.log('1' / '0') ; // ==> Infinity   🆒 🆒 🆒 
+ 
+
+
+
