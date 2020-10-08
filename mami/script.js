1904b63f3ef2596574f63064ca02d14dd4423925
carveler <3heyaberlin@gmail.com>
2020-10-08 08:28:56 +0200

    mami

---
+++ mami/script.js
@@ -0,0 +1,8 @@
+// NaN is Not a number.
+console.log(isNaN("hello"));
+console.log(isNaN(3));
+console.log(typeof NaN);
+
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
