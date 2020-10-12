809cfc9af063cfa4c1f6f011a53f91ef3e960c06
Zarzarius <zarzarius@gmail.com>
2020-10-12 16:05:09 +0200

    exercise done

--- Azael/index.js
+++ Azael/index.js
@@ -6,7 +6,31 @@
 * Check if `3` is NaN.
 * What is the type of NaN?*/
 
+let greeting = "hello";
+
+let checkGreeting = isNaN(greeting);
+
+console.log("isNaN(greeting)==>" + checkGreeting);
+
+let number = 3;
+
+let checkNumber = isNaN(number);
+
+console.log("isNaN(number)==>" + checkNumber);
+
+let checkNaN = typeof NaN;
+console.log("typeof NaN==>" + checkNaN);
+
 /*#### 2. Infinity
  * Divide Infinity by `0`.
  * Divide Infinity by Infinity.
  * Divide `1` by `0`.*/
+
+let infinite = Infinity / 0;
+console.log("infinite==>" + infinite);
+
+let infinite1 = Infinity / Infinity;
+console.log("infinite1==>" + infinite1);
+
+let infinite2 = 1 / 0;
+console.log("infinite2==>" + infinite2);
